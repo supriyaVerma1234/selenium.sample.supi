def0b930731dba4c85ac0bd8a14c10c54e902043
Mohit Verma <Mohit Verma@192.168.100.8>
2019-09-19 21:12:30 +0530

    supi addition test case

---
+++ SupiJavabasic/src/addition/addNumbers.java
@@ -0,0 +1,23 @@
+package addition;
+
+public class addNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<5;i++)
+		{
+
+			
+			for(int j=0;j<i;j++) 
+			{
+				System.out.print(j+1);
+				
+			}
+			System.out.println("  ");
+
+	}
+		
+	}
+
+}
