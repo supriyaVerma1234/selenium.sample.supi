c5802ad1bf13ed99e48b02d4eb4e2d077bb0831e
Mohit Verma <Mohit Verma@192.168.100.8>
2019-09-19 21:26:39 +0530

    editied by supi

---
+++ SupiJavabasic/src/addition/multiple.java
@@ -0,0 +1,12 @@
+package addition;
+
+public class multiple {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Supi is intelligent");
+		// TODO Auto-generated method stub
+
+	}
+
+}
